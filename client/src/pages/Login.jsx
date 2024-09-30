e1e3f916b2f426c92b495f948d8986c1970fac24
User <User@WRSD-LD309-026.dut.ac.za>
2024-09-30 10:15:39 +0200

    feat:login.jsx button added, styling added

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -8,14 +8,14 @@ function Login(){
     dispatch(verifyUser())
 
     return(
-        <div>
+        <div style={{justifyContent:"center", textAlign:"center"}}>
             <h1>Login</h1>
             <p>Please enter your login details</p>
 
             <input type="text" placeholder="Enter your Username" style={{margin:"2%"}}></input><br></br>
-            <input type="text" placeholder="Enter your Password"></input><br></br>
+            <input type="text" placeholder="Enter your Password" style={{margin:"2%"}}></input><br></br>
 
-            <button>Login</button>
+            <button style={{margin:"2%"}}>Login</button>
         </div>
     )
 }

bbaa5946ef61ec875ae7885e6a451791b5d66de4
User <User@WRSD-LD309-026.dut.ac.za>
2024-09-30 09:49:02 +0200

    feat:landing.jsx updated,login.jsx button added

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,13 +1,17 @@
+import { Link } from "react-router-dom"
+
 function Landing(){
     return(
-        <section style={{display:"flex"}}>
-            <div>
+        <section style={{display:"flex" }}>
+            <div style={{alignItems:"center", justifyContent:"center", textAlign:"center"}}>
                 <h1>
                     Welcome to ListIt. Need to get things done, ListIt!
                 </h1>
+
+                <button style={{backgroundColor:"#99dae7"}}><Link to={"/login"} style={{color:"black"}}>Login</Link></button>
             </div>
 
-            <div style={{backgroundImage:`url(${require("../assets/List.png")})`}}>
+            <div style={{background:`url(${require("../assets/List.png")})`}}>
               
             </div>
         </section>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -8,7 +8,15 @@ function Login(){
     dispatch(verifyUser())
 
     return(
-        <></>
+        <div>
+            <h1>Login</h1>
+            <p>Please enter your login details</p>
+
+            <input type="text" placeholder="Enter your Username" style={{margin:"2%"}}></input><br></br>
+            <input type="text" placeholder="Enter your Password"></input><br></br>
+
+            <button>Login</button>
+        </div>
     )
 }
 export default Login
